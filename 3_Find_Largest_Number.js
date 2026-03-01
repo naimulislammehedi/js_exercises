c9d60ad2ffadb5e80a1492eab5bf0aaa423065cd
Mehedi <mehedi.interface@gmail.com>
2026-03-01 12:41:54 +0600

    Find Smallest Number

---
+++ 3_Find_Largest_Number.js
@@ -0,0 +1,6 @@
+// Find Largest Number
+function finMax(arr) {
+    return Math.max(...arr); 
+}
+
+console.log(findMax([10, 5, 25, 8])); 
---
+++ 4_Find_Smallest_Number.js
@@ -0,0 +1,5 @@
+function findMin(arr) {
+    return Math.min(...arr); 
+}
+
+console.log(findMin([10, 3, 56, 22, 5, 89])); 
